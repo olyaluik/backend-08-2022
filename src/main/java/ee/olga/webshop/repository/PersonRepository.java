4a206e9ba14b93ff6e42c3f47183b9ba2a5a56eb
olyaluik <89607449+olyaluik@users.noreply.github.com>
2022-08-30 17:13:00 +0300

    Update PersonRepository.java

--- src/main/java/ee/olga/webshop/repository/PersonRepository.java
+++ src/main/java/ee/olga/webshop/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package ee.olga.webshop.repository;
 
 import ee.olga.webshop.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, String> {
 }
